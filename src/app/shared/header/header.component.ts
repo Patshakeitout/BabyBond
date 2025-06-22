48490308aaa720385015838b26e968ad39c36987
Patrick Schauer <patrick_schauer@t-online.de>
2025-06-22 12:02:14 +0200

    Fix: Nav links and toggle switch

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,37 +4,22 @@ import { NAV_LINKS } from '../nav-links';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  /** Navigation elements */
   navLinks = NAV_LINKS;
-
-  get whatIDoChildren() {
-    const subNavs = this.navLinks.find(
-      link => link.title === 'Was ich mache'
-    )?.children || [];
-
-    return subNavs;
-  }
-
-  get whatIAmChildren() {
-    const subNavs = this.navLinks.find(
-      link => link.title === 'Wer ich bin'
-    )?.children || [];
-
-    return subNavs;
-  }
-
-
-  /** Fires when the user clicks the menu icon */
+  
   @Output() menuToggle = new EventEmitter<void>();
-
-  linksVisible = true;
 
   toggleMenu() {
     this.menuToggle.emit();
-    this.linksVisible = !this.linksVisible;
+  }
+  
+  get whatIDoChildren() {
+    return this.navLinks.find(l => l.title === 'Was ich mache')?.children ?? [];
   }
 
+  get whatIAmChildren() {
+    return this.navLinks.find(l => l.title === 'Wer ich bin')?.children ?? [];
+  }
 }
