3372dad9de8ddc28c8a80211e1fbabbbdd0102b0
Patrick <patrick_schauer@t-online.de>
2026-05-08 12:26:47 +0200

    feat: update new dates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router'
 
 import { NAV_LINKS } from './shared/nav-links';
 
-declare let klaro: any; // Typen vom NPM-Package sind rudimentär
+declare let klaro: any;
+declare let window: any;
 
 
 @Component({
@@ -18,9 +19,11 @@ export class AppComponent implements AfterViewInit {
   constructor(public router: Router) {}
 
   ngAfterViewInit() {
-    // Klaro-Config und CSS/Skript wurden ja via angular.json geladen
     if (typeof klaro !== 'undefined') {
-      klaro.run();  // zeigt das Banner nur, wenn noch keine Einwilligung
+      const manager = klaro.getManager(window.klaroConfig);
+      if (!manager.confirmed) {
+        klaro.show(window.klaroConfig);
+      }
     }
   }
 }
